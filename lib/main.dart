17f69dc52c879c7575c5f944c7e94c82ac814f31
Badhon100 <badhonx102@gmail.com>
2023-06-04 06:02:25 +0600

    done with firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/firebase_options.dart';
 import 'package:reddit_clone/theme/pallete.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
@@ -13,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Reddit Clone',
       theme: Pallete.darkModeAppTheme,
       home: const LoginScreen(),
     );
